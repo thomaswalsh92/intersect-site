db39919737c884208e4f69ce7b8c8c5a0bd6a631
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2025-09-17 11:50:39 +0100

    Installed SASS

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React from "react";
 
 //app
 import AppCanvas from "./AppCanvas";
+import Home from "./Home";
 // import { Component } from "./Component";
 
 export default function App() {
-  return <AppCanvas />;
+  return <Home />;
 }
