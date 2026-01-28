3a5f4133aaa6487fdc5c0ca0b2c543f19dd89829
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2026-01-28 16:45:45 +0000

    Added pseudo element borders and image cycyling funciontality

--- src/WorkExplore.jsx
+++ src/WorkExplore.jsx
@@ -1,8 +1,8 @@
 //styling
 import "./scss/Home.scss";
 
-import { useEffect, useRef, useState } from "react";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+// import { ScrollSmoother } from "gsap/ScrollSmoother";
 import useWindowDimensions from "./utils/useWindowDimensions";
 
 export default function WorkExplore({ active, currentProject, closeProject }) {
@@ -17,36 +17,56 @@ export default function WorkExplore({ active, currentProject, closeProject }) {
   }, [active, closeProject]);
 
   const { width, height } = useWindowDimensions();
+  const [widthUnit, setWidthUnit] = useState();
+  const [heightUnit, setHeightUnit] = useState();
   const [railHeight, setRailHeight] = useState(null);
   const [rows, setRows] = useState(null);
+  const [imagePlaceholders, setImagePlaceholders] = useState([
+    { aspectRatio: "16 / 9", color: "red" },
+    { aspectRatio: "4 / 5", color: "green" },
+    { aspectRatio: "9 / 16", color: "blue" },
+  ]);
   const workExploreGrid = useRef(null);
 
   useEffect(() => {
-    if (!workExploreGrid.current) return;
-    const heightUnit = workExploreGrid.current.offsetHeight / 19;
+    if (!workExploreGrid.current && !active) return;
+    setHeightUnit(workExploreGrid.current.offsetHeight / 19);
+    setWidthUnit(workExploreGrid.current.offsetWidth / 38);
+  }, [workExploreGrid, active]);
+
+  useEffect(() => {
     const sixteenNineHeight = heightUnit * 13;
     const sixteenNineWidth = (sixteenNineHeight / 9) * 16;
-    const maxWidth = width * 0.7;
+    const maxWidth = width * 0.8;
     if (sixteenNineWidth > maxWidth) {
       const maxHeight = (maxWidth / 16) * 9;
       setRailHeight(maxHeight);
     }
-  }, [width, height, workExploreGrid]);
+  }, [width, height, widthUnit, heightUnit]);
 
   useEffect(() => {
-    if (!workExploreGrid.current) return;
-
     if (railHeight) {
       const heightUnit = Math.floor(workExploreGrid.current.offsetHeight / 19);
-      const scalableHeightSection = heightUnit * 15;
-      const marginsHeight = Math.floor(scalableHeightSection - railHeight);
       const railHeightUnit = Math.floor(railHeight / 13);
-
       setRows(
-        `${heightUnit}px 1fr repeat(13, ${railHeightUnit}px) 1fr repeat(3, ${heightUnit}px)`
+        `${heightUnit}px 1fr repeat(13, ${railHeightUnit}px) 1fr repeat(3, ${heightUnit}px)`,
       );
     }
   }, [railHeight, workExploreGrid]);
+
+  function handleForward() {
+    setImagePlaceholders((prev) => {
+      if (prev.length === 0) return prev;
+      return [prev[prev.length - 1], ...prev.slice(0, prev.length - 1)];
+    });
+  }
+
+  function handleBack() {
+    setImagePlaceholders((prev) => {
+      if (prev.length === 0) return prev;
+      return [...prev.slice(1), prev[0]];
+    });
+  }
 
   if (!active) return null;
   return (
@@ -121,21 +141,34 @@ export default function WorkExplore({ active, currentProject, closeProject }) {
           }}
         ></div>
         {/* ROW 3 IMAGE RAIL */}
+
         <div
           id="work-explore-image-rail"
           className="work-grid-block work-grid-end-block"
           style={{
             gridColumn: "span 38",
             gridRow: "span 13",
+            gap: widthUnit * 2,
           }}
         >
-          <div
-            style={{
-              height: "100%",
-              aspectRatio: "16 / 9",
-              background: "#ccc",
-            }}
-          ></div>
+          <button onClick={handleBack} className="work-explore-button left">
+            <span className="text-1">{`<-`}</span>
+          </button>
+          <button onClick={handleForward} className="work-explore-button right">
+            <span className="text-1">{`->`}</span>
+          </button>
+          {imagePlaceholders.map(({ aspectRatio, color }) => {
+            return (
+              <div
+                className="work-explore-image-container"
+                style={{
+                  height: "100%",
+                  aspectRatio: aspectRatio,
+                  background: color,
+                }}
+              ></div>
+            );
+          })}
         </div>
         {/* ROW 4 MARGIN */}
         <div
